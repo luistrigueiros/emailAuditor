4102fc7f1138899b0c723e894c4dbcfe0f35dda3
luist <luistrigueiros@gmail.com>
2024-01-27 21:33:51 +0000

    Initial import

---
+++ src/main/java/ie/vhi/emailAuditor/EmailAuditorApplication.java
@@ -0,0 +1,13 @@
+package ie.vhi.emailAuditor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailAuditorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailAuditorApplication.class, args);
+	}
+
+}
