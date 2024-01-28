73112fb8a69ae7612594cfa989f448eaa9b42751
luist <luistrigueiros@gmail.com>
2024-01-28 16:01:12 +0000

    Need to set jmsMessageType=object to be able to serialize the object

--- common/src/main/java/ie/vhi/emailAuditor/EmailImpl.java
+++ common/src/main/java/ie/vhi/emailAuditor/EmailImpl.java
@@ -3,9 +3,11 @@ package ie.vhi.emailAuditor;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.io.Serializable;
+
 @Getter
 @Builder
-public class EmailImpl implements Email{
+public class EmailImpl implements Email, Serializable {
     private String toAddress;
     private String fromAddress;
     private String subject;
