924cb4fc4047b9fb8cd99f2f3b31012fb8f9d10e
luist <luistrigueiros@gmail.com>
2024-01-29 12:31:56 +0000

    Downgrade to work with Java 8

--- auditor/src/main/java/ie/vhi/emailAuditor/EmailAudit.java
+++ auditor/src/main/java/ie/vhi/emailAuditor/EmailAudit.java
@@ -1,10 +1,11 @@
 package ie.vhi.emailAuditor;
 
 
-import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+
+import javax.persistence.*;
 
 @Getter
 @Setter
--- auditor/src/test/java/ie/vhi/emailAuditor/EmailAuditorApplicationTests.java
+++ auditor/src/test/java/ie/vhi/emailAuditor/EmailAuditorApplicationTests.java
@@ -2,8 +2,10 @@ package ie.vhi.emailAuditor;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class EmailAuditorApplicationTests {
 
 	@Test
